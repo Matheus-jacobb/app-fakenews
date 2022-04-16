746bafa15e30e0adaa029e65222de68f636b1b9f
Antonio Canno <antonio.canno257@gmail.com>
2022-04-15 23:33:34 -0300

    update(app-footer): icons and new option
    update(icons): using 'ionicons'

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Tab4',
+  templateUrl: 'Tab4.page.html',
+  styleUrls: ['Tab4.page.scss']
+})
+export class Tab4Page {
+
+  constructor() {}
+
+}
