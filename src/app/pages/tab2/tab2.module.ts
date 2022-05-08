cb5cb67898bc47f968af524d1bf7af092003f892
Antonio Canno <antonio.canno257@gmail.com>
2022-05-08 01:31:31 -0300

    update(tab2): added color picker

--- src/app/pages/tab2/tab2.module.ts
+++ src/app/pages/tab2/tab2.module.ts
@@ -9,6 +9,7 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
 import { ArrowCircleModule } from 'src/app/components/arrow-circle/arrow-circle.module';
 import { CreationInputModule } from 'src/app/components/creation-input/creation-input.module';
 import { HeaderModule } from 'src/app/components/header/header.module';
+import { ColorPickerModule } from 'src/app/components/color-picker/color-picker.module';
 
 @NgModule({
   imports: [
@@ -20,6 +21,7 @@ import { HeaderModule } from 'src/app/components/header/header.module';
     ArrowCircleModule,
     CreationInputModule,
     HeaderModule,
+    ColorPickerModule
   ],
   declarations: [Tab2Page]
 })
